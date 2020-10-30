a337b5300ee12aafe980810589c7fc7901fe0d7d
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-10-30 17:22:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <img src="cat2.jpg" width="200" height="121" <bt />
     <a href="https://www.youtube.com/watch?v=3VTkBuxU4yk">Click This Link!!!</a>
 
-    <h2>Funny Story</h2>
+    <h2>K/DA - MORE[Lyrics] ให้ผมเต็มเถอะผมหาเรื่องตลกไม่ได้5555555555</h2>
     <p>
       Akali that girl, 'Kali go grr 'Kali don't stop, 'Kali don't skrrt 'Kali
       got a job, 'Kali go to work 뜨거워 언제나, don't get burnt 넘쳐 흘러
@@ -19,7 +19,12 @@
       than 필요없는 시험들, 답은 이미 But all of my numbers are talkin', babe
       블루마블 Mrs. 모노폴리 (Ooh) 너는 – 종이 돈이 나는 real money (Hey) 필요
       없대 너네들이 많은 돈들 Go get it, go get it, go get it, the mission
-      눈을못떼 모두 그래 너도 그래 (Oh) 'Cause I got it different
+      눈을못떼 모두 그래 너도 그래 (Oh) 'Cause I got it different More Know I
+      got it, so here you go (Let's go) You look like you could use some more
+      (More) Know I got it and never runnin' low (Low) Yeah, I got more than
+      enough, add it up and away (Ayy, ayy) You know I got it like bomb-bomb,
+      blow your mind Never givin' less and that's how it'll stay (Ayy, ayy) You
+      know I got it like all day, all the time
     </p>
   </body>
 </html>
