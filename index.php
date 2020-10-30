481c0c8ac2c0023f24e54d9fbf885d33829e8a7f
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-10-30 17:24:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,15 @@
     <img src="cat2.jpg" width="200" height="121" <bt />
     <a href="https://www.youtube.com/watch?v=3VTkBuxU4yk">Click This Link!!!</a>
 
-    <h2>K/DA - MORE[Lyrics] ให้ผมเต็มเถอะผมหาเรื่องตลกไม่ได้5555555555</h2>
+    <h3>ITF-Lab</h3>
     <p>
-      Akali that girl, 'Kali go grr 'Kali don't stop, 'Kali don't skrrt 'Kali
-      got a job, 'Kali go to work 뜨거워 언제나, don't get burnt 넘쳐 흘러
-      more than a buffet 난 죽여 주잖아 like I'm Buffy 누가 감히 on my Huffy 거-
-      문열지마 that's a rough day (Woo) I'm givin' you more 'cause I'm greater
-      than 필요없는 시험들, 답은 이미 But all of my numbers are talkin', babe
-      블루마블 Mrs. 모노폴리 (Ooh) 너는 – 종이 돈이 나는 real money (Hey) 필요
-      없대 너네들이 많은 돈들 Go get it, go get it, go get it, the mission
-      눈을못떼 모두 그래 너도 그래 (Oh) 'Cause I got it different More Know I
-      got it, so here you go (Let's go) You look like you could use some more
-      (More) Know I got it and never runnin' low (Low) Yeah, I got more than
-      enough, add it up and away (Ayy, ayy) You know I got it like bomb-bomb,
-      blow your mind Never givin' less and that's how it'll stay (Ayy, ayy) You
-      know I got it like all day, all the time
+      สำหรับ LAB ในวัน ศุกร์ที่ 30 ตุลาคม 2563 นศ. ต้องเตรียมสิ่งต่างๆ ให้พร้อม
+      - เปิดใช้ GITHUB ให้เรียบร้อย - Account ของสถาบัน ( 63070xxx@kmitl.ac.th )
+      ต้องสามารถใช้งานได้ หากใครจำ password ไม่ได้ ให้ติดต่อ
+      สำนักบริการคอมพิวเตอร์ชั้น 1 - ให้เตรียม หูฟัง มาด้วย เพราะว่า Lab จะเป็น
+      คลิป VDO ให้เปิดดู แล้วทำตาม ขอขอบคุณในความร่วมมือครับ
     </p>
   </body>
 </html>
+
 
