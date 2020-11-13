6efa08740f654b1954514577c9c50dc23862edb4
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-13 17:37:49 +0700

    Add files via upload

---
+++ show.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lab-itf-63070211-kantapak.mysql.database.azure.com', 'ronaldofocus7@lab-itf-63070211-kantapak
+', 'Ronaldo77', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook2');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
