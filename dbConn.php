2f199cf12d5158bae651cbb6b152d8a38ff12666
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-20 21:35:07 +0700

    Create dbConn.php

---
+++ dbConn.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = mysqli_connect("lab-itf-63070211-kantapak.mysql.database.azure.com","ronaldofocus7@lab-itf-63070211-kantapak","Ronaldo77","ITFLab");
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
