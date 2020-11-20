5ddb0a589fe0456e8ec8e83899fea13081268423
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-20 20:28:51 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,19 +1,22 @@
 <?php
+/*
+Author: Javed Ur Rehman
+Website: https://www.allphptricks.com/
+*/
+ 
 require('db.php');
 include("auth.php");
+
 $status = "";
-if(isset($_POST['new']) && $_POST['new']==1){
-    $trn_date = date("Y-m-d H:i:s");
-    $name =$_REQUEST['name'];
-    $age = $_REQUEST['age'];
-    $submittedby = $_SESSION["username"];
-    $ins_query="insert into new_record
-    (`trn_date`,`name`,`age`,`submittedby`)values
-    ('$trn_date','$name','$age','$submittedby')";
-    mysqli_query($con,$ins_query)
-    or die(mysql_error());
-    $status = "New Record Inserted Successfully.
-    </br></br><a href='view.php'>View Inserted Record</a>";
+if(isset($_POST['new']) && $_POST['new']==1)
+{
+$trn_date = date("Y-m-d H:i:s");
+$name =$_REQUEST['name'];
+$age = $_REQUEST['age'];
+$submittedby = $_SESSION["username"];
+$ins_query="insert into new_record (`trn_date`,`name`,`age`,`submittedby`) values ('$trn_date','$name','$age','$submittedby')";
+mysqli_query($con,$ins_query) or die(mysql_error());
+$status = "New Record Inserted Successfully.</br></br><a href='view.php'>View Inserted Record</a>";
 }
 ?>
 <!DOCTYPE html>
@@ -25,9 +28,8 @@ if(isset($_POST['new']) && $_POST['new']==1){
 </head>
 <body>
 <div class="form">
-<p><a href="dashboard.php">Dashboard</a> 
-| <a href="view.php">View Records</a> 
-| <a href="logout.php">Logout</a></p>
+<p><a href="dashboard.php">Dashboard</a> | <a href="view.php">View Records</a> | <a href="logout.php">Logout</a></p>
+
 <div>
 <h1>Insert New Record</h1>
 <form name="form" method="post" action=""> 
@@ -37,6 +39,11 @@ if(isset($_POST['new']) && $_POST['new']==1){
 <p><input name="submit" type="submit" value="Submit" /></p>
 </form>
 <p style="color:#FF0000;"><?php echo $status; ?></p>
+
+<br /><br /><br /><br />
+<a href="https://www.allphptricks.com/insert-view-edit-and-delete-record-from-database-using-php-and-mysqli/">Tutorial Link</a> <br /><br />
+For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/">AllPHPTricks.com</a>
+<br /><br />Watch TV Abroad via Best VPNs Guide Visit: <a href="https://www.bestvpnsguide.com/">BestVPNsGuide.com</a>
 </div>
 </div>
 </body>
--- view.php
+++ view.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Author: Javed Ur Rehman
+Website: https://www.allphptricks.com/
+*/
+ 
 require('db.php');
 include("auth.php");
 ?>
@@ -11,19 +16,11 @@ include("auth.php");
 </head>
 <body>
 <div class="form">
-<p><a href="index.php">Home</a> 
-| <a href="insert.php">Insert New Record</a> 
-| <a href="logout.php">Logout</a></p>
+<p><a href="index.php">Home</a> | <a href="insert.php">Insert New Record</a> | <a href="logout.php">Logout</a></p>
 <h2>View Records</h2>
 <table width="100%" border="1" style="border-collapse:collapse;">
 <thead>
-<tr>
-<th><strong>S.No</strong></th>
-<th><strong>Name</strong></th>
-<th><strong>Age</strong></th>
-<th><strong>Edit</strong></th>
-<th><strong>Delete</strong></th>
-</tr>
+<tr><th><strong>S.No</strong></th><th><strong>Name</strong></th><th><strong>Age</strong></th><th><strong>Edit</strong></th><th><strong>Delete</strong></th></tr>
 </thead>
 <tbody>
 <?php
@@ -31,19 +28,15 @@ $count=1;
 $sel_query="Select * from new_record ORDER BY id desc;";
 $result = mysqli_query($con,$sel_query);
 while($row = mysqli_fetch_assoc($result)) { ?>
-<tr><td align="center"><?php echo $count; ?></td>
-<td align="center"><?php echo $row["name"]; ?></td>
-<td align="center"><?php echo $row["age"]; ?></td>
-<td align="center">
-<a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
-</td>
-<td align="center">
-<a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a>
-</td>
-</tr>
+<tr><td align="center"><?php echo $count; ?></td><td align="center"><?php echo $row["name"]; ?></td><td align="center"><?php echo $row["age"]; ?></td><td align="center"><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a></td><td align="center"><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td></tr>
 <?php $count++; } ?>
 </tbody>
 </table>
+
+<br /><br /><br /><br />
+<a href="https://www.allphptricks.com/insert-view-edit-and-delete-record-from-database-using-php-and-mysqli/">Tutorial Link</a> <br /><br />
+For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/">AllPHPTricks.com</a>
+<br /><br />Watch TV Abroad via Best VPNs Guide Visit: <a href="https://www.bestvpnsguide.com/">BestVPNsGuide.com</a>
 </div>
 </body>
 </html>
