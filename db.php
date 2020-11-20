ee2ba300d2ab7dc6f9906ed4041d3017918fd7e9
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-20 20:32:15 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$con = mysqli_connect("lab-itf-63070211-kantapak.mysql.database.azure.com","ronaldofocus7@lab-itf-63070211-kantapak","Ronaldo77","register");
+$con = mysqli_connect("lab-itf-63070211-kantapak.mysql.database.azure.com","ronaldofocus7@lab-itf-63070211-kantapak","Ronaldo77","ITFLab", 3306);
 // Check connection
 if (mysqli_connect_errno())
   {
