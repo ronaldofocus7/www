e7cce510fef6e187eb33d728fa595e7a96043d57
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-20 21:23:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,22 @@
-<?php
-
-include "insert.php"; // Using database connection file here
-
-$id = $_GET['id']; // get id through query string
-
-$del = mysqli_query($db,"delete from tblemp where id = '$id'"); // delete query
-
-if($del)
-{
-    mysqli_close($db); // Close connection
-    header("location:all_records.php"); // redirects to all records page
-    exit;	
+$id = $_GET['id'];
+//Connect DB
+//Create query based on the ID passed from you table
+//query : delete where Staff_id = $id
+// on success delete : redirect the page to original page using header() method
+$dbname = "ITFLab";
+$conn = mysqli_connect("lab-itf-63070211-kantapak.mysql.database.azure.com", "ronaldofocus7@lab-itf-63070211-kantapak", "Ronaldo77", $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-else
-{
-    echo "Error deleting record"; // display error message if not delete
+
+// sql to delete a record
+$sql = "DELETE FROM Bookings WHERE Staff_ID = $id"; 
+
+if (mysqli_query($conn, $sql)) {
+    mysqli_close($conn);
+    header('Location: show.php'); //If book.php is your main page where you list your all records
+    exit;
+} else {
+    echo "Error deleting record";
 }
-?>
