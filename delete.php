88c4fd5f883f12e33fdf436046ef17bf33938554
ronaldofocus7 <73693981+ronaldofocus7@users.noreply.github.com>
2020-11-20 20:45:33 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,9 +4,9 @@ Author: Javed Ur Rehman
 Website: https://www.allphptricks.com/
 */
 
-require('db.php');
+require('show.php');
 $id=$_REQUEST['id'];
 $query = "DELETE FROM new_record WHERE id=$id"; 
 $result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: view.php"); 
+header("Location: show.php"); 
 ?>
